0527f5c2483c0f72600adf220fb27621e5a20a1d
Bianca <bianca.mularoni@me.com>
2024-07-04 16:18:41 +0100

    Add extra reducer for the addQuiz action

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -7,7 +7,12 @@ const topicsSlice = createSlice({
         addTopic: (state, action) => {
             const { id, name, icon } = action.payload;
             state.topics[id] = {id, name, icon, quizIds: []}; // select the "topics" object inside the "topics" slice, and creates a new property with the id as key, and the object as value
-
+        }
+    },
+    extraReducers: {
+        'quizzes/addQuiz': (state, action) => { // In "extraReducers" because that state is updated in response to an action type ("addQuiz") defined elsewhere ("quizzesSlice")
+            const { id, topicId } = action.payload;
+            state.topics[topicId].quizIds.push(id);
         }
     }
 });
@@ -15,7 +20,7 @@ const topicsSlice = createSlice({
 // Selector
 export const selectTopics = state => state.topics.topics; // The state has a "topics" property, which inside has a "topics" object: "state{topics{topics{...}}}"
 
-// Export Action creator
-export const { addTopic } = topicsSlice.actions; 
-// Export the Reducer
-export default topicsSlice.reducer;
+// Export Action Creator
+export const { addTopic } = topicsSlice.actions; // The extraReducer depends on the "addQuiz" action so the action creator is exported in the quizzesSlice.js file not here, and the associated case reducer is already exported in the slice reducer below
+// Export the Slice Reducer
+export default topicsSlice.reducer; 
