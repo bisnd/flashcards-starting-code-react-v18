08ad082f0a6cbb065d42ef8640051429664fce41
Bianca <bianca.mularoni@me.com>
2024-07-04 14:49:25 +0100

    Create topicsSlice with addTopic action

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,2 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {topics: {}},
+    reducers: {
+        addTopic: (state, action) => {
+            const { id, name, icon } = action.payload;
+            state.topics[id] = {id, name, icon, quizIds: []}; // select the "topics" object inside the "topics" slice, and creates a new property with the id as key, and the object as value
 
+        }
+    }
+});
+
+// Selector
+export const selectTopics = state => state.topics.topics; // The state has a "topics" property, which inside has a "topics" object: "state{topics{topics{...}}}"
+
+// Export Action creator
+export const { addTopic } = topicsSlice.actions; 
+// Export the Reducer
+export default topicsSlice.reducer;
