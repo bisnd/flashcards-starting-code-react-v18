bb231229bd1e97bf78cc946f6322b67396144f7e
Bianca <bianca.mularoni@me.com>
2024-07-04 17:43:34 +0100

    add slice reducers to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
+import topicsReducer from "../features/topics/topicsSlice"; // It was a default export so it can be named on import
+import quizzesReducer from "../features/quizzes/quizzesSlice";
+import cardsReducer from "../features/cards/cardsSlice";
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsReducer,
+    quizzes: quizzesReducer,
+    cards: cardsReducer
   },
 });
